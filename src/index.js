f6b0d76039ca3edaaf56cd7385bac6a9d89be10f
= <=>
2023-05-18 03:34:51 +0900

    프론트 환경 세팅 완료

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>, root
+);  
+
+// ReactDOM.render(React 컴포넌트, 부모 엘리먼트) 메소드
+// => React 컴포넌트의 구성 요소들을 부모 엘리먼트 아래에 추가한 DOM 트리를 만듦
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
